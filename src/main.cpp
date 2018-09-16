1914ad79bfa81c649b324e0095596db8f37f0fe8
ArtemBoyarintsev <boyarintsev95@gmail.com>
2018-09-16 23:26:41 +0700

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -39,12 +39,12 @@ int main()
     // Откройте 
     // https://www.khronos.org/registry/OpenCL/sdk/1.2/docs/man/xhtml/
     // Нажмите слева: "OpenCL Runtime" -> "Query Platform Info" -> "clGetPlatformIDs"
-    // Прочитайте документацию clGetPlatformIDs и убедитесь что этот способ узнать сколько есть платформ соответствует документации:
+    // Прочитайте документацию clGetPlatformIDs и убедитесь что этот способ узнать сколько есть платформ соответствует документации: Done
     cl_uint platformsCount = 0;
     OCL_SAFE_CALL(clGetPlatformIDs(0, nullptr, &platformsCount));
     std::cout << "Number of OpenCL platforms: " << platformsCount << std::endl;
 
-    // Тот же метод используется для того чтобы получить идентификаторы всех платформ - сверьтесь с документацией, что это сделано верно:
+    // Тот же метод используется для того чтобы получить идентификаторы всех платформ - сверьтесь с документацией, что это сделано верно: Done
     std::vector<cl_platform_id> platforms(platformsCount);
     OCL_SAFE_CALL(clGetPlatformIDs(platformsCount, platforms.data(), nullptr));
 
@@ -56,7 +56,7 @@ int main()
         // Не забывайте проверять коды ошибок с помощью макроса OCL_SAFE_CALL
         size_t platformNameSize = 0;
         OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, 0, nullptr, &platformNameSize));
-        // TODO 1.1
+        // TODO 1.1 - Done
         // Попробуйте вместо CL_PLATFORM_NAME передать какое-нибудь случайное число - например 239
         // Т.к. это некорректный идентификатор параметра платформы - то метод вернет код ошибки
         // Макрос OCL_SAFE_CALL заметит это, и кинет ошибку с кодом
@@ -67,21 +67,77 @@ int main()
         // Затем откройте документацию по clGetPlatformInfo и в секции Errors найдите ошибку, с которой столкнулись
         // в документации подробно объясняется, какой ситуации соответствует данная ошибка, и это позволит проверив код понять чем же вызвана данная ошибка (не корректным аргументом param_name)
         // Обратите внимание что в этом же libs/clew/CL/cl.h файле указаны всевоможные defines такие как CL_DEVICE_TYPE_GPU и т.п.
+        //
 
         // TODO 1.2
         // Аналогично тому как был запрошен список идентификаторов всех платформ - так и с названием платформы, теперь, когда известна длина названия - его можно запросить:
         std::vector<unsigned char> platformName(platformNameSize, 0);
-        // clGetPlatformInfo(...);
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, platformNameSize, platformName.data(), nullptr));
         std::cout << "    Platform name: " << platformName.data() << std::endl;
 
         // TODO 1.3
-        // Запросите и напечатайте так же в консоль вендора данной платформы
+        size_t platformVendorSize = 0;
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, 0, nullptr, &platformVendorSize));
+
+        std::vector<unsigned char> platformVendor(platformNameSize, 0);
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, platformVendorSize, platformVendor.data(), nullptr));
+        std::cout << "    Platform name: " << platformVendor.data() << std::endl;
 
         // TODO 2.1
         // Запросите число доступных устройств данной платформы (аналогично тому как это было сделано для запроса числа доступных платформ - см. секцию "OpenCL Runtime" -> "Query Devices")
         cl_uint devicesCount = 0;
+        OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, nullptr , &devicesCount));
+        std::cout << "    Number of  devices: " << devicesCount << std::endl;
 
+        std::vector<cl_device_id> devices(devicesCount);
+        OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, devicesCount, devices.data(), nullptr));
+
+
+        auto getInfo = [](const cl_device_id& device, cl_device_info paramName) {
+            size_t paramNameSize = 0;
+            OCL_SAFE_CALL(clGetDeviceInfo(device, paramName, 0, nullptr, &paramNameSize));
+            std::vector<unsigned char> deviceName(paramNameSize, 0);
+            OCL_SAFE_CALL(clGetDeviceInfo(device, paramName, paramNameSize, deviceName.data() ,0));
+
+            return deviceName;
+        };
         for (int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex) {
+            cl_device_id device = devices[deviceIndex];
+
+            std::vector<unsigned char> deviceName = getInfo(device, CL_DEVICE_NAME);
+            std::cout << "        Device name: " << deviceName.data() << std::endl;
+
+            std::vector<unsigned char> driverVersion = getInfo(device, CL_DRIVER_VERSION);
+            std::cout << "            Driver version : " << driverVersion.data() << std::endl;
+
+
+            cl_ulong maxMemory;
+            OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(maxMemory), &maxMemory, nullptr));
+            std::cout << "            Max allocated memoty is : " << maxMemory / (1024 * 1024) << "Mb" << std::endl;
+
+            cl_device_type deviceType;
+            OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_TYPE, sizeof(cl_device_type), &deviceType, nullptr));
+            switch (deviceType)
+            {
+                case CL_DEVICE_TYPE_CPU:
+                    std::cout << "            Device type is CPU" << std::endl;
+                    break;
+
+                case CL_DEVICE_TYPE_GPU:
+                    std::cout << "            Device type is GPU"  << std::endl;
+                    break;
+                default:
+                    std::cout << "            Device type is unknwon..."  << std::endl;
+                    break;
+
+            }
+
+            cl_bool imagesSupported;
+            OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_IMAGE_SUPPORT, sizeof(imagesSupported), &imagesSupported, nullptr));
+            if (imagesSupported)
+                std::cout << "            Images are supported " << std::endl;
+
+
             // TODO 2.2
             // Запросите и напечатайте в консоль:
             // - Название устройства
